123d8ccceb856d04f489f8bf3f9b52a942f155b5
SergioViacheslaev <internetwalker@yandex.ru>
2019-07-02 14:33:58 +0300

    Add Assembly pugin

--- hw01-maven/src/main/java/HelloOtus.java
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -5,6 +5,27 @@ import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Examples.
+ * To start the application:
+ * mvn package
+ * java -cp ./target/L01-maven.jar ru.otus.l011.Main //java.lang.NoClassDefFoundError: com/google/common/collect/Lists
+ * java -jar ./target/L01-maven.jar
+ * java -cp ./target/L01-maven.jar;{home_dir}\.m2\repository\com\google\guava\guava\23.0\guava-23.0.jar ru.otus.l011.Main
+ *
+ * To unzip the jar:
+ * 7z x -oJAR ./target/L01-maven.jar
+ * unzip -d JAR ./target/L01-maven.jar
+ * unzip -l target/L01-maven.jar
+ *
+ *
+ * To build:
+ * mvn package
+ * mvn clean compile
+ * mvn assembly:single
+ * mvn clean compile assembly:single
+ */
+
 public class HelloOtus {
     public static void main(String[] args) {
         System.out.println("Hello World from Java " + System.getProperty("java.version"));
