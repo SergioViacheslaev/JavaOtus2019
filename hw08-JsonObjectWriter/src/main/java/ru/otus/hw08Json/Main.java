082e728e4ecdf4614b0440635fa35bee62e0b14f
SergioViacheslaev <internetwalker@yandex.ru>
2019-09-05 10:28:48 +0300

    Initital, Add libraries

---
+++ hw08-JsonObjectWriter/src/main/java/ru/otus/hw08Json/Main.java
@@ -0,0 +1,14 @@
+package ru.otus.hw08Json;
+
+import com.google.gson.Gson;
+
+/**
+ * @author Sergei Viacheslaev
+ */
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+
+        
+    }
+}
