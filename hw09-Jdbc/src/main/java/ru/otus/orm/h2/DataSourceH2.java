d371d430a422b361a13cdca8cff4d65ad1e3bb45
SergioViacheslaev <internetwalker@yandex.ru>
2019-09-11 13:22:36 +0300

    Initial, add H2 database

---
+++ hw09-Jdbc/src/main/java/ru/otus/orm/h2/DataSourceH2.java
@@ -0,0 +1,65 @@
+package ru.otus.orm.h2;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+/** H2 - DataBase
+ *
+ * To keep the content of an in-memory database as long
+ * as the virtual machine is alive,use jdbc:h2:mem:test;DB_CLOSE_DELAY=-1.
+ */
+public class DataSourceH2 implements DataSource {
+  private static final String URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+
+  @Override
+  public Connection getConnection() throws SQLException {
+    Connection connection = DriverManager.getConnection(URL);
+    connection.setAutoCommit(false);
+    return connection;
+  }
+
+  @Override
+  public Connection getConnection(String username, String password) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public PrintWriter getLogWriter() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void setLogWriter(PrintWriter out) {
+    throw new UnsupportedOperationException();
+
+  }
+
+  @Override
+  public int getLoginTimeout() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void setLoginTimeout(int seconds) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Logger getParentLogger() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public <T> T unwrap(Class<T> iface) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean isWrapperFor(Class<?> iface) {
+    throw new UnsupportedOperationException();
+  }
+}
