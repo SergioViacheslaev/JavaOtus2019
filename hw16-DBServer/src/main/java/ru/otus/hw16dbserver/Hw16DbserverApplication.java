69e343939e45a630001982bd03bee86db4111b84
SergioViacheslaev <internetwalker@yandex.ru>
2019-11-20 12:51:57 +0300

    Add hw16-DBServer module

---
+++ hw16-DBServer/src/main/java/ru/otus/hw16dbserver/Hw16DbserverApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw16dbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw16DbserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw16DbserverApplication.class, args);
+    }
+
+}
