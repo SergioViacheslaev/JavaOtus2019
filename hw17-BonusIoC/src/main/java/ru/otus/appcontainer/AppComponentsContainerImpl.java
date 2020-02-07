ebbcc2e677fdf4c7b2652ca455e4fa3599ed3e8e
SergioViacheslaev <internetwalker@yandex.ru>
2020-02-07 17:12:04 +0300

    Refactor

--- hw17-BonusIoC/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw17-BonusIoC/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,7 +1,5 @@
 package ru.otus.appcontainer;
 
-import org.reflections.Reflections;
-import org.reflections.scanners.MethodAnnotationsScanner;
 import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
@@ -9,17 +7,17 @@ import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final Map<String, Object> appComponentsByName = new HashMap<>();
-    private final Map<String, String> componentNameByClassName = new HashMap<>();
+    private final Map<String, Object> appComponentsByClassName = new HashMap<>();
     private final MethodComparator methodComparator = new MethodComparator();
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        String componentName = componentNameByClassName.get(componentClass.getSimpleName());
-        return (C) appComponentsByName.get(componentName);
+        return (C) appComponentsByClassName.get(componentClass.getSimpleName());
     }
 
     @Override
@@ -33,9 +31,8 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
-        List<Method> componentMethods = parseConfigClass(configClass);
-        Object configInstance = createInstance(configClass).orElseThrow();
+        List<Method> componentMethods = getAppComponentMethods(configClass);
+        Object configInstance = createInstance(configClass).orElseThrow(() -> new RuntimeException("Can't create config Instance"));
         registerComponents(configInstance, componentMethods);
     }
 
@@ -52,62 +49,50 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
             Class<?> componentClass = appComponentMethod.getReturnType();
             Class<?>[] methodArgs = appComponentMethod.getParameterTypes();
             String componentName = annotation.name();
-            componentNameByClassName.put(componentClass.getSimpleName(), componentName);
 
-            Object registeredComponent;
-            if (methodArgs.length == 0) {
-                registeredComponent = invokeMethod(appComponentMethod, configInstance).orElseThrow();
-            } else {
-                Object[] initArgs = getInitArgs(methodArgs);
-                registeredComponent = invokeMethod(appComponentMethod, configInstance, initArgs).orElseThrow();
-            }
+            Object[] initArgs = getInitArgs(methodArgs);
+            Object registeredComponent = invokeMethod(appComponentMethod, configInstance, initArgs).
+                    orElseThrow(() -> new RuntimeException(String.format("Can't register Component: %s", componentName)));
+
             appComponentsByName.put(componentName, registeredComponent);
+            appComponentsByClassName.put(componentClass.getSimpleName(), registeredComponent);
         }
 
     }
 
-    private List<Method> parseConfigClass(Class<?> configClass) {
-        Set<Method> appComponentMethods = getAppComponentMethods(configClass);
-        List<Method> sortedComponentMethods = new ArrayList<>(appComponentMethods);
-        sortedComponentMethods.sort(methodComparator);
-        return sortedComponentMethods;
-    }
-
     private Optional<Object> createInstance(Class<?> clazz) {
-        Optional<Object> instance = Optional.empty();
+        Optional<Object> instance;
         try {
             instance = Optional.of(clazz.getDeclaredConstructor().newInstance());
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Create Instance Exception: ", e);
         }
         return instance;
     }
 
     private Optional<Object> invokeMethod(Method method, Object object, Object... args) {
-        Optional<Object> result = Optional.empty();
+        Optional<Object> result;
         try {
             result = Optional.ofNullable(method.invoke(object, args));
         } catch (IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Invoke method Exception: ", e);
         }
         return result;
     }
 
 
     private Object[] getInitArgs(Class<?>[] componentConstructorArgsTypes) {
-        List<Object> componentArgs = new ArrayList<>();
-        for (Class<?> argType : componentConstructorArgsTypes) {
-            Object argObject = appComponentsByName.get(componentNameByClassName.get(argType.getSimpleName()));
-            componentArgs.add(argObject);
+        Object[] componentArgs = new Object[componentConstructorArgsTypes.length];
+        for (int i = 0; i < componentArgs.length; i++) {
+            componentArgs[i] = appComponentsByClassName.get(componentConstructorArgsTypes[i].getSimpleName());
         }
-        return componentArgs.toArray();
+        return componentArgs;
     }
 
-    private Set<Method> getAppComponentMethods(Class<?> configClass) {
-        Reflections reflections = new Reflections(
-                configClass,
-                new MethodAnnotationsScanner());
-        return reflections.getMethodsAnnotatedWith(AppComponent.class);
+    private List<Method> getAppComponentMethods(Class<?> configClass) {
+        return Arrays.stream(configClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(methodComparator).collect(Collectors.toList());
     }
 
     private static class MethodComparator implements Comparator<Method> {
@@ -119,5 +104,4 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
             return Integer.compare(orderMethod1, orderMethod2);
         }
     }
-
 }
